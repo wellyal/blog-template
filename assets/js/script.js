ab58cb51db8d60cbf164b59934d390a59a33ac2e
Wellyngton Amaral <ifa.wellyngton.amaral@locaweb.com.br>
2016-05-02 23:02:32 -0300

    adding style for github icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,31 @@ cfd.menubar = (function() {
   'use strict';
 
   function redirect() {
-    $("#postslist").on('click', function(){
-      $('#posts').removeClass('cfd-hidden');
-      $('#panel').addClass('cfd-hidden');
+    $( "#postslist" ).on( 'click', function() {
+      $( "#posts" ).removeClass( 'cfd-hidden' );
+      $( "#panel" ).addClass( 'cfd-hidden' );
     });
 
-    $("#createpost").on('click', function(){
-      $('#panel').removeClass('cfd-hidden');
-      $('#posts').addClass('cfd-hidden');
+    $( "#createpost" ).on( 'click', function() {
+      $( "#panel" ).removeClass( 'cfd-hidden' );
+      $( "#posts" ).addClass( 'cfd-hidden' );
+    });
+  }
+
+  function overGithubIcon() {
+    $( '.cfd-header-disclaimer' ).on({
+      mouseover:function () {
+       $( "#githubicon" ).removeClass( 'cfd-hidden' );
+      },
+      mouseleave:function() {
+       $( "#githubicon" ).addClass( 'cfd-hidden' );
+      }
     });
   }
 
   function init() {
     redirect();
+    overGithubIcon();
   }
 
   return {
