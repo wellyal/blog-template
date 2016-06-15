3056098d091a868f607875322f0f8153ab160fcf
Wellyngton Amaral <wellington.amaral.ufc@gmail.com>
2016-06-15 19:52:31 -0300

    updating ident tabs for js and html files with editorconfig

--- assets/js/article.js
+++ assets/js/article.js
@@ -3,18 +3,9 @@ var cfd = cfd || {};
 cfd.article = (function() {
   'use strict';
 
-  function upPage() {
-
-  }
-
-  function init() {
-    upPage();
-  }
-
   return {
-    init: init
   };
 
 }(jQuery));
 
-cfd.article.init();
+/*cfd.article.init();*/
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,40 +3,55 @@ var cfd = cfd || {};
 cfd.main = (function() {
   'use strict';
 
-  function redirect() {
-    $( "#postslist" ).on( 'click', function() {
-      $( "#posts" ).removeClass( 'cfd-hidden' );
-      $( "#panel" ).addClass( 'cfd-hidden' );
-    });
+	$(document).ready(function(){
+		var _paq = _paq || [];
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
 
-    $( "#createpost" ).on( 'click', function() {
-      $( "#panel" ).removeClass( 'cfd-hidden' );
-      $( "#posts" ).addClass( 'cfd-hidden' );
-    });
-  }
+		(function() {
+			var u="http://cluster-piwik.locaweb.com.br/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', 7506]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	});
 
-  function overGithubIcon() {
-    $( '.cfd-header-disclaimer' ).on({
-      mouseover:function () {
-       $( "#githubicon" ).removeClass( 'cfd-hidden' );
-      },
-      mouseleave:function() {
-       $( "#githubicon" ).addClass( 'cfd-hidden' );
-      }
-    });
-  }
+	function redirect () {
+		$( "#postslist" ).on( 'click', function() {
+			$( "#posts" ).removeClass( 'cfd-hidden' );
+			$( "#panel" ).addClass( 'cfd-hidden' );
+		});
 
-  function goTo() {
-    $( '.cfd-post-item' ).on( 'click', function(){
-      window.location="templates/article.html";
-    });
-  }
+		$( "#createpost" ).on( 'click', function() {
+			$( "#panel" ).removeClass( 'cfd-hidden' );
+			$( "#posts" ).addClass( 'cfd-hidden' );
+		});
+	}
 
-  function init() {
-    redirect();
-    overGithubIcon();
-    goTo();
-  }
+	function overGithubIcon () {
+		$( '.cfd-header-disclaimer' ).on({
+			mouseover:function () {
+				$( "#githubicon" ).removeClass( 'cfd-hidden' );
+			},
+
+			mouseleave:function() {
+				$( "#githubicon" ).addClass( 'cfd-hidden' );
+			}
+		});
+	}
+
+	function goTo () {
+		$( '.cfd-post-item' ).on( 'click', function(){
+			window.location="templates/article.html";
+		});
+	}
+
+	function init () {
+		redirect();
+		overGithubIcon();
+		goTo();
+	}
 
   return {
     init: init
